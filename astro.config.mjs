6d746025f078dbaa4eec59f6f2b903b270f8e303
SokiSama <sokisama0@gmail.com>
2025-10-12 15:07:56 +0800

    修改了bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: "https://sokisama.github.io/",
 
-	base: "my-repo",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
